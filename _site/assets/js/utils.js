7d245e653ae108037e002fcc8134b4696d9f8fc4
joosthuizen <65812622+johnoos@users.noreply.github.com>
2026-05-18 10:35:06 +0200

    now with latest CV encompassing both academic research and AI training groups of personas

--- _site/assets/js/utils.js
+++
@@ -1,9 +0,0 @@
-export function showSpinner() {
-  const spinner = document.getElementById('page-spinner');
-  if (spinner) spinner.classList.add('active');
-}
-
-export function hideSpinner() {
-  const spinner = document.getElementById('page-spinner');
-  if (spinner) spinner.classList.remove('active');
-}
